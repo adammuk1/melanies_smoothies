fba341244e9a1ab303b647ed6053493e23a604ff
adammuk1 <135766516+adammuk1@users.noreply.github.com>
2025-01-29 22:47:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,14 @@ st.write("The name on your Smoothie will be:", name_on_order)
 # Fetch fruit options from Snowflake
 try:
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-    st.dataframe(my_dataframe, use_container_width=True)
-    st.stop()
-except Exception as e:
-    st.error(f"Error fetching fruit options: {e}")
+    # st.dataframe(my_dataframe, use_container_width=True)
+    # st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 
 
 
